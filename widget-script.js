97232c0f4822afa858672d9a9500b20df1b7a2e9
Patrick Scott <patrickscott1712@gmail.com>
2016-02-05 21:09:33 +0000

    Update widget-script.js

--- widget-script.js
+++ widget-script.js
@@ -108,6 +108,7 @@ function drawChart(place_data, place_name) {
 		if ((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i))) {
         		var ori = window.orientation;
         		width = (ori==90 || ori==-90) ? screen.height : screen.width;
+        		width = width - margin;
     		}
 		else {width = parseInt($('#chart-container').width(), 10) - margin;}
 		
